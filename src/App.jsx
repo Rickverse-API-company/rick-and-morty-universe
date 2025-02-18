bcf46d09138e1cbe5f275661f13533179177a60d
Fantastic George <jorislam@gmail.com>
2025-02-18 09:05:23 -0300

    big changes to data handling and routing to get characterdetails working

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 import HomePage from './pages/homepage'
+import CharacterDetails from './components/characterdetails'
 import Navbar from './components/navbar'
 import Footer from './components/footer'
 
-
 function App() {
-
+  const [characters, setCharacters] = useState([]);
 
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path="/*" element={<h1>Page not found</h1>} />
-    </Routes>
-    <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage characters={characters} setCharacters={setCharacters} />} />
+        <Route path="/character/:id" element={<CharacterDetails characters={characters} />} />
+        <Route path="*" element={<h1>Page not found</h1>} />
+      </Routes>
+      <Footer />
     </>
   )
 }
