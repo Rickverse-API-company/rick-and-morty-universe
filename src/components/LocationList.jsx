5b036b7156d0c149f720e6c4ad9370d79f5de320
Fantastic George <jorislam@gmail.com>
2025-02-18 11:08:01 -0300

    location data is correctly being processed

--- src/components/LocationList.jsx
+++ src/components/LocationList.jsx
@@ -6,11 +6,18 @@ const LocationList = ({ locations }) => {
     return (
         <div className="location-list">
             <h2>Locations</h2>
+    
             <ul>
                 {locations.map((location) => (
+                    <div key={location.id}>
                     <li key={location.id}>{location.name}</li>
+                    <p>{location.name}</p>
+                    <p>{location.type}</p>
+                    <p>{location.dimension}</p>
+                    </div>
                 ))}
             </ul>
+        
         </div>
     )
 }
